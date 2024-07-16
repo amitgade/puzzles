e64c7feafe91889fd8f59e949bf6d7aaa7bc1b33
amitgade <amitgaade@gmail.com>
2024-07-16 16:58:06 +0530

    action-Add user input prompt

--- src/com/amitgade/numbers/ArmstrongNumber.java
+++ src/com/amitgade/numbers/ArmstrongNumber.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class ArmstrongNumber {
     public static void main(String[] args) {
+        System.out.print("Enter a number: ");
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
 
